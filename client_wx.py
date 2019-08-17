c02d69d8f26da36c81efe747d90849a2e67502cc
MrSedan <elpashevs@bk.ru>
2019-08-17 20:49:52 +0300

    UPD

--- client_wx.py
+++ client_wx.py
@@ -1,31 +1,109 @@
 import wx
+from twisted.internet import wxreactor
+
+wxreactor.install()
+
+from twisted.internet import reactor
+from twisted.internet.protocol import ClientFactory
+from twisted.protocols.basic import LineOnlyReceiver
 
 
-class MainWindow(wx.Frame):
-    button: wx.Button
+class Client(LineOnlyReceiver):
+    """Класс для обработки сообщений"""
+
+    factory: 'Connector'
+
+    def connectionMade(self):
+        """Обработчик установки соединения с сервером"""
+
+        self.factory.window.protocol = self  # записали в окно приложения текущий протокол
+
+    def lineReceived(self, line):
+        """Обработчик получения новой строки от сервера"""
+
+        message = line.decode()  # раскодируем
+        self.factory.window.text_box.AppendText(f"{message}\n")  # добавим в поле сообщений
+
+
+class Connector(ClientFactory):
+    """Класс для установки подключения к серверу"""
+
+    window: 'ChatWindow'
+    protocol = Client
+
+    def __init__(self, app_window):
+        """Запоминаем окно приложения в конструкторе для обращения"""
+
+        self.window = app_window
+
+
+class ChatWindow(wx.Frame):
+    """Класс для запуска графического приложения"""
+
+    protocol: Client  # протокол подключения
     text_box: wx.TextCtrl
+    message_box: wx.TextCtrl
+    submit_button: wx.Button
 
-    def __init__(self, *args, **kw):
-        super(MainWindow, self).__init__(*args, **kw)
+    def __init__(self):
+        """Инициализация окна и обработчиков"""
 
-        sizer = wx.BoxSizer(wx.VERTICAL)
+        super().__init__(
+            None,
+            title='Chat window',
+            size=wx.Size(350, 500)
+        )
 
-        self.text_box = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        self.button = wx.Button(self, label="Test button")
+        self.build_widgets()
 
-        sizer.Add(self.text_box, flags=wx.SizerFlags(1).Border(wx.ALL, 10).Expand())
-        sizer.Add(self.button)
+    def build_widgets(self):
+        """Построение интерфейса"""
 
-        self.button.Bind(wx.EVT_BUTTON, self.update_text)
+        panel = wx.BoxSizer(wx.VERTICAL)
 
-        self.SetSizer(sizer)
+        # компоненты
+        self.text_box = wx.TextCtrl(self, style=wx.TE_MULTILINE | wx.TE_READONLY)
+        self.message_box = wx.TextCtrl(self)
+        self.message_box.SetHint("Your message")
+        self.submit_button = wx.Button(self, label="Submit")
 
-    def update_text(self, event):
-        self.text_box.AppendText(f"Button clicked! {event}")
+        # установка расположения
+        panel.Add(self.text_box, flags=wx.SizerFlags(1).Expand())
+        panel.Add(self.message_box, flags=wx.SizerFlags().Expand().Border(wx.ALL, 5))
+        panel.Add(self.submit_button, flags=wx.SizerFlags().Expand().Border(wx.LEFT | wx.BOTTOM | wx.RIGHT, 5))
+
+        # обработчики
+        self.submit_button.Bind(wx.EVT_BUTTON, self.send_message)
+
+        # применяем расположение в окне
+        self.SetSizer(panel)
+
+    def send_message(self, event):
+        """Обработчик отправки сообщения"""
+
+        text_message = self.message_box.GetValue()
+
+        self.protocol.sendLine(text_message.encode())  # отправили
+        self.message_box.SetValue('')  # сбросили текст в поле
 
 
 if __name__ == '__main__':
+    # создали приложение
     app = wx.App()
-    frm = MainWindow(None, title="Precol")
-    frm.Show()
-    app.MainLoop()
+
+    # создали окно
+    window = ChatWindow()
+    window.Show()
+
+    # зарегистрировали в реакторе
+    # app.MainLoop()
+    reactor.registerWxApp(app)
+
+    # стандартное подключение
+    reactor.connectTCP(
+        "localhost",
+        9090,
+        Connector(window)
+    )
+
+    reactor.run()
